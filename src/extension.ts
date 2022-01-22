89d38e40edf063d357bd10456a2884d95b0ffd0b
Justin Chu <justinchuby@users.noreply.github.com>
2022-01-21 20:52:51 -0800

    Format

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,11 @@
   * */
 
 /** imports */
-import * as vscode from 'vscode';
-import InteractiveWebviewGenerator from './features/interactiveWebview';
-import PreviewPanel from './features/previewPanel';
+import * as vscode from "vscode";
+import InteractiveWebviewGenerator from "./features/interactiveWebview";
+import PreviewPanel from "./features/previewPanel";
 
-const DOT = 'dot';
+const DOT = "dot";
 
 /** global vars */
 
@@ -19,62 +19,62 @@ const DOT = 'dot';
 
 /** event funcs */
 function onActivate(context: vscode.ExtensionContext) {
-  const graphvizView = new InteractiveWebviewGenerator(context);
+    const graphvizView = new InteractiveWebviewGenerator(context);
 
-  vscode.workspace.onDidChangeTextDocument((event) => {
-    if (event.document.languageId == DOT || event.document.fileName.trim().toLowerCase().endsWith('.dot')) {
-      const panel = graphvizView.getPanel(event.document.uri);
-      if (panel) {
-        panel.requestRender(event.document.getText());
-      }
-    }
-  }, null, context.subscriptions);
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        if (event.document.languageId == DOT || event.document.fileName.trim().toLowerCase().endsWith(".dot")) {
+            const panel = graphvizView.getPanel(event.document.uri);
+            if (panel) {
+                panel.requestRender(event.document.getText());
+            }
+        }
+    }, null, context.subscriptions);
 
-  context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => {
-    if (doc.languageId == DOT || doc.fileName.trim().toLowerCase().endsWith('.dot')) {
-      const panel = graphvizView.getPanel(doc.uri);
-      if (panel) {
-        panel.requestRender(doc.getText());
-      }
-    }
-  }));
+    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => {
+        if (doc.languageId == DOT || doc.fileName.trim().toLowerCase().endsWith(".dot")) {
+            const panel = graphvizView.getPanel(doc.uri);
+            if (panel) {
+                panel.requestRender(doc.getText());
+            }
+        }
+    }));
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('graphviz-interactive-preview.preview.beside', (args) => {
-      // take document or string; default active editor if
-      args = args || {};
-      const options = {
-        document: args.document,
-        content: args.content,
-        callback: args.callback,
-        allowMultiplePanels: args.allowMultiplePanels,
-        title: args.title,
-        search: args.search,
-      };
+    context.subscriptions.push(
+        vscode.commands.registerCommand("graphviz-interactive-preview.preview.beside", (args) => {
+            // take document or string; default active editor if
+            args = args || {};
+            const options = {
+                document: args.document,
+                content: args.content,
+                callback: args.callback,
+                allowMultiplePanels: args.allowMultiplePanels,
+                title: args.title,
+                search: args.search,
+            };
 
-      if (!options.content && !options.document && vscode.window.activeTextEditor?.document) {
-        options.document = vscode.window.activeTextEditor.document;
-      }
+            if (!options.content && !options.document && vscode.window.activeTextEditor?.document) {
+                options.document = vscode.window.activeTextEditor.document;
+            }
 
-      if (!options.content && options.document) {
-        options.content = options.document.getText();
-      }
+            if (!options.content && options.document) {
+                options.content = options.document.getText();
+            }
 
-      graphvizView.revealOrCreatePreview(vscode.ViewColumn.Beside, options.document, options)
-        .then((webpanel : PreviewPanel) => {
-          // trigger dot render on page load success
-          // just in case webpanel takes longer to load, wait for page to ping back and perform action
-          webpanel.waitingForRendering = options.content;
-          webpanel.search = options.search;
+            graphvizView.revealOrCreatePreview(vscode.ViewColumn.Beside, options.document, options)
+                .then((webpanel : PreviewPanel) => {
+                    // trigger dot render on page load success
+                    // just in case webpanel takes longer to load, wait for page to ping back and perform action
+                    webpanel.waitingForRendering = options.content;
+                    webpanel.search = options.search;
 
-          // allow caller to handle messages by providing them with the newly created webpanel
-          // e.g. caller can override webpanel.handleMessage = function(message){};
-          if (options.callback) {
-            options.callback(webpanel);
-          }
-        });
-    }),
-  );
+                    // allow caller to handle messages by providing them with the newly created webpanel
+                    // e.g. caller can override webpanel.handleMessage = function(message){};
+                    if (options.callback) {
+                        options.callback(webpanel);
+                    }
+                });
+        }),
+    );
 }
 
 /* exports */
