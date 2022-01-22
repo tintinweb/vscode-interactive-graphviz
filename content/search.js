e732bf392bf2ec66577cb324247f2ee9127b0e5d
Justin Chu <justinchuby@users.noreply.github.com>
2022-01-22 11:06:17 -0800

    Merge from master and format

--- content/search.js
+++ content/search.js
@@ -1,41 +1,40 @@
 function findEdges(text, searchFunction) {
-    var $set = $();
+    const $set = $();
     gv.edges().each((index, edge) => {
         if (edge.textContent && searchFunction(text, edge.textContent)) {
             $set.push(edge);
         }
-    })
+    });
     return $set;
 }
 
 function findNodes(text, searchFunction, nodeName = true, nodeLabel = true) {
-    var $set = $()
+    const $set = $();
 
     const nodes = gv.nodesByName();
-    for (let [nodeID, node] of Object.entries(nodes)) {
-        if ((nodeName && searchFunction(text, nodeID)) ||
-            (nodeLabel &&
-                (node.textContent && searchFunction(text, node.textContent) || !node.textContent && !nodeName && searchFunction(text, nodeID)))) {
-            $set.push(node)
+    for (const [nodeID, node] of Object.entries(nodes)) {
+        if ((nodeName && searchFunction(text, nodeID))
+            || (nodeLabel
+                && (node.textContent && searchFunction(text, node.textContent) || !node.textContent && !nodeName && searchFunction(text, nodeID)))) {
+            $set.push(node);
         }
     }
     return $set;
 }
 
 function findClusters(text, searchFunction, clusterName = true, clusterLabel = true) {
-    var $set = $()
+    const $set = $();
 
     const clusters = gv.clustersByName();
-    for (let [clusterID, cluster] of Object.entries(clusters)) {
-        if ((clusterName && searchFunction(text, clusterID)) ||
-            (clusterLabel &&
-                (cluster.textContent && searchFunction(text, cluster.textContent) || !cluster.textContent && !clusterName && searchFunction(text, clusterID)))) {
-            $set.push(cluster)
+    for (const [clusterID, cluster] of Object.entries(clusters)) {
+        if ((clusterName && searchFunction(text, clusterID))
+            || (clusterLabel
+                && (cluster.textContent && searchFunction(text, cluster.textContent) || !cluster.textContent && !clusterName && searchFunction(text, clusterID)))) {
+            $set.push(cluster);
         }
     }
     return $set;
 }
-
 
 // main search function (is also used by API call)
 function search(text, mode = "highlight", options = {}) {
@@ -47,7 +46,7 @@ function search(text, mode = "highlight", options = {}) {
         edgeLabel: options.edgeLabel,
         clusterName: options.clusterName,
         clusterLabel: options.clusterLabel,
-    }
+    };
 
     if (mode === "search") {
         if (Object.keys(options).length > 0) {
@@ -59,31 +58,29 @@ function search(text, mode = "highlight", options = {}) {
 
     let searchFunction;
     if (opt.type === "exact") {
-        searchFunction = (search, str) => {
-            return str.trim() === search.trim()
-        };
+        searchFunction = (search, str) => str.trim() === search.trim();
     } else if (opt.type === "included") {
         searchFunction = (search, str) => str.trim().indexOf(search) !== -1;
     } else if (opt.type === "regex") {
         searchFunction = (search, str) => {
-            let regex = new RegExp(search);
+            const regex = new RegExp(search);
             return !!str.trim().match(regex);
-        }
+        };
     }
 
     if (mode === "highlight") {
-        var $edges = $();
+        let $edges = $();
         if (opt.edgeLabel) {
             $edges = findEdges(text, searchFunction);
         }
 
-        var $nodes = $();
+        let $nodes = $();
         if (opt.nodeLabel || opt.nodeName) {
             $nodes = findNodes(text, searchFunction, opt.nodeName, opt.nodeLabel);
         }
 
-        var $clusters = $();
-        if(opt.clusterLabel || opt.clusterName) {
+        let $clusters = $();
+        if (opt.clusterLabel || opt.clusterName) {
             $clusters = findClusters(text, searchFunction, opt.clusterName, opt.clusterLabel);
         }
 
@@ -95,7 +92,7 @@ function search(text, mode = "highlight", options = {}) {
             nodes: $nodes,
             edges: $edges,
             clusters: $clusters,
-        }
+        };
     }
 
     sendMessage("Invalid search Mode!");
@@ -103,32 +100,31 @@ function search(text, mode = "highlight", options = {}) {
 
 function findString(str) {
     if (parseInt(navigator.appVersion) < 4) return;
-    var strFound;
+    let strFound;
     if (window.find) {
         // CODE FOR BROWSERS THAT SUPPORT window.find
         strFound = self.find(str);
         if (strFound && self.getSelection && !self.getSelection().anchorNode) {
-            strFound = self.find(str)
+            strFound = self.find(str);
         }
         if (!strFound) {
-            strFound = self.find(str, 0, 1)
-            while (self.find(str, 0, 1)) continue
+            strFound = self.find(str, 0, 1);
+            while (self.find(str, 0, 1)) continue;
         }
     } else if (navigator.appName.indexOf("Microsoft") != -1) {
         // EXPLORER-SPECIFIC CODE
         if (TRange != null) {
-            TRange.collapse(false)
-            strFound = TRange.findText(str)
-            if (strFound) TRange.select()
+            TRange.collapse(false);
+            strFound = TRange.findText(str);
+            if (strFound) TRange.select();
         }
         if (TRange == null || strFound == 0) {
-            TRange = self.document.body.createTextRange()
-            strFound = TRange.findText(str)
-            if (strFound) TRange.select()
+            TRange = self.document.body.createTextRange();
+            strFound = TRange.findText(str);
+            if (strFound) TRange.select();
         }
     }
     if (!strFound) {
         // flash box
     }
-    return;
-};
+}
--- updateDependencies.js
+++ updateDependencies.js
@@ -1,54 +1,54 @@
-const fs = require('fs');
+const fs = require("fs");
 
-if (!fs.existsSync('content/dist/')){
-  fs.mkdirSync('content/dist/');
+if (!fs.existsSync("content/dist/")) {
+    fs.mkdirSync("content/dist/");
 }
 
-fs.copyFile('node_modules/@hpcc-js/wasm/dist/graphvizlib.wasm', 'content/dist/graphvizlib.wasm', (err) => {
-  if (err) throw err;
-  console.log('graphvizlib.wasm was copied to content/dist');
+fs.copyFile("node_modules/@hpcc-js/wasm/dist/graphvizlib.wasm", "content/dist/graphvizlib.wasm", (err) => {
+    if (err) throw err;
+    console.log("graphvizlib.wasm was copied to content/dist");
 });
 
-fs.copyFile('node_modules/@hpcc-js/wasm/dist/index.min.js', 'content/dist/hpccjswasm.js', (err) => {
-  if (err) throw err;
-  console.log('hpccjswasm.js was copied to content/dist');
+fs.copyFile("node_modules/@hpcc-js/wasm/dist/index.min.js", "content/dist/hpccjswasm.js", (err) => {
+    if (err) throw err;
+    console.log("hpccjswasm.js was copied to content/dist");
 });
 
-fs.copyFile('node_modules/jquery/dist/jquery.min.js', 'content/dist/jquery.min.js', (err) => {
-  if (err) throw err;
-  console.log('jquery.min.js was copied to content/dist');
+fs.copyFile("node_modules/jquery/dist/jquery.min.js", "content/dist/jquery.min.js", (err) => {
+    if (err) throw err;
+    console.log("jquery.min.js was copied to content/dist");
 });
 
-fs.copyFile('node_modules/jquery-mousewheel/jquery.mousewheel.js', 'content/dist/jquery.mousewheel.js', (err) => {
-  if (err) throw err;
-  console.log('jquery.mousewheel.js was copied to content/dist');
+fs.copyFile("node_modules/jquery-mousewheel/jquery.mousewheel.js", "content/dist/jquery.mousewheel.js", (err) => {
+    if (err) throw err;
+    console.log("jquery.mousewheel.js was copied to content/dist");
 });
 
-fs.copyFile('node_modules/jquery-color/dist/jquery.color.min.js', 'content/dist/jquery.color.min.js', (err) => {
-  if (err) throw err;
-  console.log('jquery.color.min.js was copied to content/dist');
+fs.copyFile("node_modules/jquery-color/dist/jquery.color.min.js", "content/dist/jquery.color.min.js", (err) => {
+    if (err) throw err;
+    console.log("jquery.color.min.js was copied to content/dist");
 });
 
-fs.copyFile('node_modules/d3/dist/d3.min.js', 'content/dist/d3.min.js', (err) => {
-  if (err) throw err;
-  console.log('d3.min.js was copied to content/dist');
+fs.copyFile("node_modules/d3/dist/d3.min.js", "content/dist/d3.min.js", (err) => {
+    if (err) throw err;
+    console.log("d3.min.js was copied to content/dist");
 });
 
-fs.copyFile('node_modules/d3-graphviz/build/d3-graphviz.min.js', 'content/dist/d3-graphviz.min.js', (err) => {
-  if (err) throw err;
-  console.log('d3-graphviz.min.js was copied to content/dist');
+fs.copyFile("node_modules/d3-graphviz/build/d3-graphviz.min.js", "content/dist/d3-graphviz.min.js", (err) => {
+    if (err) throw err;
+    console.log("d3-graphviz.min.js was copied to content/dist");
 });
 
-fs.copyFile('node_modules/@vscode/webview-ui-toolkit/dist/toolkit.min.js', 'content/dist/webviewuitoolkit.min.js', (err) => {
-  if (err) throw err;
-  console.log('webviewuitoolkit.min.js was copied to content/dist');
+fs.copyFile("node_modules/@vscode/webview-ui-toolkit/dist/toolkit.min.js", "content/dist/webviewuitoolkit.min.js", (err) => {
+    if (err) throw err;
+    console.log("webviewuitoolkit.min.js was copied to content/dist");
 });
 
-fs.copyFile('node_modules/@vscode/codicons/dist/codicon.css', 'content/dist/codicon.css', (err) => {
-  if (err) throw err;
-  console.log('codicon.css was copied to content/dist');
+fs.copyFile("node_modules/@vscode/codicons/dist/codicon.css", "content/dist/codicon.css", (err) => {
+    if (err) throw err;
+    console.log("codicon.css was copied to content/dist");
 });
-fs.copyFile('node_modules/@vscode/codicons/dist/codicon.ttf', 'content/dist/codicon.ttf', (err) => {
-  if (err) throw err;
-  console.log('codicon.ttf was copied to content/dist');
+fs.copyFile("node_modules/@vscode/codicons/dist/codicon.ttf", "content/dist/codicon.ttf", (err) => {
+    if (err) throw err;
+    console.log("codicon.ttf was copied to content/dist");
 });
