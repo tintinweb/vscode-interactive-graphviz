b8fe1a0026f29b68d9d1c46099da33c28a397b23
Justin Chu <justinchuby@users.noreply.github.com>
2022-01-31 16:29:05 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "airbnb-base",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: [
+        "@typescript-eslint",
+    ],
+    rules: {
+        indent: ["error", 4],
+        quotes: ["error", "double"],
+    },
 };
