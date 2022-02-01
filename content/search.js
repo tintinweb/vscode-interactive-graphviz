ea9ed30393c7077fd31d51b738566c07113eb262
Justin Chu <justinchuby@users.noreply.github.com>
2022-01-31 16:28:13 -0800

    Revert "Run eslint"
    This reverts commit 27565f941dc174fff4b80af2bfda870be90c521b.

--- content/search.js
+++ content/search.js
@@ -1,130 +1,134 @@
 function findEdges(text, searchFunction) {
-  const $set = $();
-  gv.edges().each((index, edge) => {
-    if (edge.textContent && searchFunction(text, edge.textContent)) {
-      $set.push(edge);
-    }
-  });
-  return $set;
+    var $set = $();
+    gv.edges().each((index, edge) => {
+        if (edge.textContent && searchFunction(text, edge.textContent)) {
+            $set.push(edge);
+        }
+    })
+    return $set;
 }
 
 function findNodes(text, searchFunction, nodeName = true, nodeLabel = true) {
-  const $set = $();
+    var $set = $()
 
-  const nodes = gv.nodesByName();
-  for (const [nodeID, node] of Object.entries(nodes)) {
-    if ((nodeName && searchFunction(text, nodeID))
-            || (nodeLabel
-                && (node.textContent && searchFunction(text, node.textContent) || !node.textContent && !nodeName && searchFunction(text, nodeID)))) {
-      $set.push(node);
+    const nodes = gv.nodesByName();
+    for (let [nodeID, node] of Object.entries(nodes)) {
+        if ((nodeName && searchFunction(text, nodeID)) ||
+            (nodeLabel &&
+                (node.textContent && searchFunction(text, node.textContent) || !node.textContent && !nodeName && searchFunction(text, nodeID)))) {
+            $set.push(node)
+        }
     }
-  }
-  return $set;
+    return $set;
 }
 
 function findClusters(text, searchFunction, clusterName = true, clusterLabel = true) {
-  const $set = $();
+    var $set = $()
 
-  const clusters = gv.clustersByName();
-  for (const [clusterID, cluster] of Object.entries(clusters)) {
-    if ((clusterName && searchFunction(text, clusterID))
-            || (clusterLabel
-                && (cluster.textContent && searchFunction(text, cluster.textContent) || !cluster.textContent && !clusterName && searchFunction(text, clusterID)))) {
-      $set.push(cluster);
+    const clusters = gv.clustersByName();
+    for (let [clusterID, cluster] of Object.entries(clusters)) {
+        if ((clusterName && searchFunction(text, clusterID)) ||
+            (clusterLabel &&
+                (cluster.textContent && searchFunction(text, cluster.textContent) || !cluster.textContent && !clusterName && searchFunction(text, clusterID)))) {
+            $set.push(cluster)
+        }
     }
-  }
-  return $set;
+    return $set;
 }
 
+
 // main search function (is also used by API call)
-function search(text, mode = 'highlight', options = {}) {
-  const opt = {
-    type: options.type || 'exact',
-    direction: options.direction || 'bidirectional',
-    nodeName: options.nodeName,
-    nodeLabel: options.nodeLabel,
-    edgeLabel: options.edgeLabel,
-    clusterName: options.clusterName,
-    clusterLabel: options.clusterLabel,
-  };
-
-  if (mode === 'search') {
-    if (Object.keys(options).length > 0) {
-      sendMessage("Options are not available in mode 'search'!");
-    }
-    findString(text);
-    return;
-  }
-
-  let searchFunction;
-  if (opt.type === 'exact') {
-    searchFunction = (search, str) => str.trim() === search.trim();
-  } else if (opt.type === 'included') {
-    searchFunction = (search, str) => str.trim().indexOf(search) !== -1;
-  } else if (opt.type === 'regex') {
-    searchFunction = (search, str) => {
-      const regex = new RegExp(search);
-      return !!str.trim().match(regex);
-    };
-  }
-
-  if (mode === 'highlight') {
-    let $edges = $();
-    if (opt.edgeLabel) {
-      $edges = findEdges(text, searchFunction);
+function search(text, mode = "highlight", options = {}) {
+    const opt = {
+        type: options.type || "exact",
+        direction: options.direction || "bidirectional",
+        nodeName: options.nodeName,
+        nodeLabel: options.nodeLabel,
+        edgeLabel: options.edgeLabel,
+        clusterName: options.clusterName,
+        clusterLabel: options.clusterLabel,
     }
 
-    let $nodes = $();
-    if (opt.nodeLabel || opt.nodeName) {
-      $nodes = findNodes(text, searchFunction, opt.nodeName, opt.nodeLabel);
+    if (mode === "search") {
+        if (Object.keys(options).length > 0) {
+            sendMessage("Options are not available in mode 'search'!");
+        }
+        findString(text);
+        return;
     }
 
-    let $clusters = $();
-    if (opt.clusterLabel || opt.clusterName) {
-      $clusters = findClusters(text, searchFunction, opt.clusterName, opt.clusterLabel);
+    let searchFunction;
+    if (opt.type === "exact") {
+        searchFunction = (search, str) => {
+            return str.trim() === search.trim()
+        };
+    } else if (opt.type === "included") {
+        searchFunction = (search, str) => str.trim().indexOf(search) !== -1;
+    } else if (opt.type === "regex") {
+        searchFunction = (search, str) => {
+            let regex = new RegExp(search);
+            return !!str.trim().match(regex);
+        }
     }
 
-    if (!opt.edgeLabel && !opt.nodeLabel && !opt.nodeName && !opt.clusterName && !opt.clusterLabel) {
-      sendMessage('No search option chosen!');
+    if (mode === "highlight") {
+        var $edges = $();
+        if (opt.edgeLabel) {
+            $edges = findEdges(text, searchFunction);
+        }
+
+        var $nodes = $();
+        if (opt.nodeLabel || opt.nodeName) {
+            $nodes = findNodes(text, searchFunction, opt.nodeName, opt.nodeLabel);
+        }
+
+        var $clusters = $();
+        if(opt.clusterLabel || opt.clusterName) {
+            $clusters = findClusters(text, searchFunction, opt.clusterName, opt.clusterLabel);
+        }
+
+        if (!opt.edgeLabel && !opt.nodeLabel && !opt.nodeName && !opt.clusterName && !opt.clusterLabel) {
+            sendMessage("No search option chosen!");
+        }
+
+        return {
+            nodes: $nodes,
+            edges: $edges,
+            clusters: $clusters,
+        }
     }
 
-    return {
-      nodes: $nodes,
-      edges: $edges,
-      clusters: $clusters,
-    };
-  }
-
-  sendMessage('Invalid search Mode!');
+    sendMessage("Invalid search Mode!");
 }
 
 function findString(str) {
-  if (parseInt(navigator.appVersion) < 4) return;
-  let strFound;
-  if (window.find) {
-    // CODE FOR BROWSERS THAT SUPPORT window.find
-    strFound = self.find(str);
-    if (strFound && self.getSelection && !self.getSelection().anchorNode) {
-      strFound = self.find(str);
+    if (parseInt(navigator.appVersion) < 4) return;
+    var strFound;
+    if (window.find) {
+        // CODE FOR BROWSERS THAT SUPPORT window.find
+        strFound = self.find(str);
+        if (strFound && self.getSelection && !self.getSelection().anchorNode) {
+            strFound = self.find(str)
+        }
+        if (!strFound) {
+            strFound = self.find(str, 0, 1)
+            while (self.find(str, 0, 1)) continue
+        }
+    } else if (navigator.appName.indexOf("Microsoft") != -1) {
+        // EXPLORER-SPECIFIC CODE
+        if (TRange != null) {
+            TRange.collapse(false)
+            strFound = TRange.findText(str)
+            if (strFound) TRange.select()
+        }
+        if (TRange == null || strFound == 0) {
+            TRange = self.document.body.createTextRange()
+            strFound = TRange.findText(str)
+            if (strFound) TRange.select()
+        }
     }
     if (!strFound) {
-      strFound = self.find(str, 0, 1);
-      while (self.find(str, 0, 1)) continue;
+        // flash box
     }
-  } else if (navigator.appName.indexOf('Microsoft') != -1) {
-    // EXPLORER-SPECIFIC CODE
-    if (TRange != null) {
-      TRange.collapse(false);
-      strFound = TRange.findText(str);
-      if (strFound) TRange.select();
-    }
-    if (TRange == null || strFound == 0) {
-      TRange = self.document.body.createTextRange();
-      strFound = TRange.findText(str);
-      if (strFound) TRange.select();
-    }
-  }
-  if (!strFound) {
-    // flash box
-  }
-}
+    return;
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,80 +1,84 @@
+'use strict';
 /**
  * @author github.com/tintinweb
  * @license GPLv3
  *
   * */
 
+
 /** imports */
 import * as vscode from 'vscode';
 import InteractiveWebviewGenerator from './features/interactiveWebview';
 import PreviewPanel from './features/previewPanel';
-
 const DOT = 'dot';
 
 /** global vars */
 
 /** classdecs */
 
+
 /** funcdecs */
+
 
 /** event funcs */
 function onActivate(context: vscode.ExtensionContext) {
-  const graphvizView = new InteractiveWebviewGenerator(context);
+    const graphvizView = new InteractiveWebviewGenerator(context);
 
-  vscode.workspace.onDidChangeTextDocument((event) => {
-    if (event.document.languageId == DOT || event.document.fileName.trim().toLowerCase().endsWith('.dot')) {
-      const panel = graphvizView.getPanel(event.document.uri);
-      if (panel) {
-        panel.requestRender(event.document.getText());
-      }
-    }
-  }, null, context.subscriptions);
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (event.document.languageId==DOT || event.document.fileName.trim().toLowerCase().endsWith(".dot")) {
+            let panel = graphvizView.getPanel(event.document.uri);
+            if(panel){
+                panel.requestRender(event.document.getText());
+            }
+        }
+    }, null, context.subscriptions);
 
-  context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((doc) => {
-    if (doc.languageId == DOT || doc.fileName.trim().toLowerCase().endsWith('.dot')) {
-      const panel = graphvizView.getPanel(doc.uri);
-      if (panel) {
-        panel.requestRender(doc.getText());
-      }
-    }
-  }));
+    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(doc => {
+        if (doc.languageId==DOT || doc.fileName.trim().toLowerCase().endsWith(".dot")) {
+            let panel = graphvizView.getPanel(doc.uri);
+            if(panel){
+                panel.requestRender(doc.getText());
+            }
+        }
+    }));
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('graphviz-interactive-preview.preview.beside', (args) => {
-      // take document or string; default active editor if
-      args = args || {};
-      const options = {
-        document: args.document,
-        content: args.content,
-        callback: args.callback,
-        allowMultiplePanels: args.allowMultiplePanels,
-        title: args.title,
-        search: args.search,
-      };
+    context.subscriptions.push(
+        vscode.commands.registerCommand('graphviz-interactive-preview.preview.beside', (args) => {
+            // take document or string; default active editor if
+            args = args || {};
+            let options = {
+                document: args.document,
+                content: args.content,
+                callback: args.callback,
+                allowMultiplePanels: args.allowMultiplePanels,
+                title: args.title,
+                search: args.search,
+            };
 
-      if (!options.content && !options.document && vscode.window.activeTextEditor?.document) {
-        options.document = vscode.window.activeTextEditor.document;
-      }
+            if(!options.content && !options.document && vscode.window.activeTextEditor?.document){
+                options.document = vscode.window.activeTextEditor.document;
+            }
 
-      if (!options.content && options.document) {
-        options.content = options.document.getText();
-      }
+            if(!options.content && options.document){
+                options.content = options.document.getText();
+            }
 
-      graphvizView.revealOrCreatePreview(vscode.ViewColumn.Beside, options.document, options)
-        .then((webpanel : PreviewPanel) => {
-          // trigger dot render on page load success
-          // just in case webpanel takes longer to load, wait for page to ping back and perform action
-          webpanel.waitingForRendering = options.content;
-          webpanel.search = options.search;
+            graphvizView.revealOrCreatePreview(vscode.ViewColumn.Beside, options.document, options)
+                .then((webpanel : PreviewPanel) => {
+                    //trigger dot render on page load success
+                    //just in case webpanel takes longer to load, wait for page to ping back and perform action
+                    webpanel.waitingForRendering = options.content;
+                    webpanel.search = options.search;
 
-          // allow caller to handle messages by providing them with the newly created webpanel
-          // e.g. caller can override webpanel.handleMessage = function(message){};
-          if (options.callback) {
-            options.callback(webpanel);
-          }
-        });
-    }),
-  );
+                    // allow caller to handle messages by providing them with the newly created webpanel
+                    // e.g. caller can override webpanel.handleMessage = function(message){};
+                    if(options.callback) {
+                        options.callback(webpanel);
+                    }
+                });
+
+        })
+    );
 }
 
 /* exports */
