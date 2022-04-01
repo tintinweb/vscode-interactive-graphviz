8cc6406845f9c77b20540d5f3bef4243bb96b845
bigbug <gutbrod.markus@googlemail.com>
2022-04-01 14:58:57 +0200

    Show Diagnostic errors in the editor (fixes #75)

---
+++ src/language/diagnosticCollection.ts
@@ -0,0 +1,5 @@
+import { DiagnosticCollection, languages } from "vscode";
+
+const diagnosticCollection: DiagnosticCollection = languages.createDiagnosticCollection();
+
+export default diagnosticCollection;
