b88758bcdf22ba5b5a66b2a97907b63f418ba0f4
bigbug <gutbrod.markus@googlemail.com>
2022-04-13 23:34:07 +0200

    Symbols, Rename and References

---
+++ src/language/SymbolProvider.ts
@@ -0,0 +1,262 @@
+import {
+  DocumentSymbolProvider,
+  Location,
+  Position,
+  Range,
+  ReferenceProvider,
+  RenameProvider,
+  SymbolInformation,
+  SymbolKind,
+  TextDocument,
+  WorkspaceEdit,
+} from "vscode";
+
+export default class SymbolProvider
+implements
+    DocumentSymbolProvider,
+    RenameProvider,
+    ReferenceProvider {
+  // eslint-disable-next-line class-methods-use-this
+  private lineIterator(
+    document: TextDocument,
+    regex: RegExp,
+    // eslint-disable-next-line no-unused-vars
+    func: (lineNumber: number, captures: RegExpExecArray, offset: number) => SymbolInformation[],
+  ) {
+    let symbols: SymbolInformation[] = [];
+    for (let line = 0; line < document.lineCount; line += 1) {
+      const t = document.lineAt(line).text;
+      let match;
+      // eslint-disable-next-line no-cond-assign
+      while ((match = regex.exec(t)) != null) {
+        const offset = match.index + 1;
+        const res = func(line, match, offset);
+        symbols = symbols.concat(res);
+      }
+    }
+    return symbols;
+  }
+
+  // find node definitions
+  // /node\s*\[.*\]\s([\w\d]+|"(?:[^"\\]|\\.)*")/g
+  private findExplicitNodeDefinition(document: TextDocument): SymbolInformation[] {
+    const regex = /node\s*\[.*\]\s([\w\d]+|"(?:[^"\\]|\\.)*")/g;
+
+    return this.lineIterator(
+      document,
+      regex,
+      (
+        lineNumber: number,
+        capture: RegExpExecArray,
+        offset: number,
+      ): SymbolInformation[] => [new SymbolInformation(
+        capture[1],
+        SymbolKind.Variable,
+        "",
+        new Location(
+          document.uri,
+          new Range(
+            new Position(
+              lineNumber,
+              offset + capture[0].length - capture[1].length,
+            ),
+            new Position(
+              lineNumber,
+              offset + capture[0].length,
+            ),
+          ),
+        ),
+      )],
+    );
+  }
+
+  // find inline nodes
+  // /^\s*((([\w\d]+)\s*)+)(?!.*(=|->|{|\[))/g
+  private findNodeDefinition(document: TextDocument): SymbolInformation[] {
+    const regex = /^\s*((([\w\d]+|"(?:[^"\\]|\\.)*")\s*)+)(?!.*(=|->|{|\[))/g;
+    const regexSymbol = /([\w\d]+|"(?:[^"\\]|\\.)*")/g;
+
+    return this.lineIterator(
+      document,
+      regex,
+      (
+        lineNumber: number,
+        captures: RegExpExecArray,
+        offset: number,
+      ): SymbolInformation[] => {
+        const symbols: SymbolInformation[] = [];
+        let symbol;
+        // eslint-disable-next-line no-cond-assign
+        while ((symbol = regexSymbol.exec(captures[0])) != null) {
+          symbols.push(
+            new SymbolInformation(
+              symbol[0],
+              SymbolKind.Variable,
+              "",
+              new Location(
+                document.uri,
+                new Range(
+                  new Position(lineNumber, offset + symbol.index - 1),
+                  new Position(lineNumber, offset + symbol.index - 1 + symbol[0].length),
+                ),
+              ),
+            ),
+          );
+        }
+        return symbols;
+      },
+    );
+  }
+
+  // find Nodes with config
+  private findNodeDefinitionWithConfig(document: TextDocument) : SymbolInformation[] {
+    const regex = /(?!\s*node)^\s*(([\w\d]+|"(?:[^"\\]|\\.)*"))\s*\[/g;
+
+    return this.lineIterator(
+      document,
+      regex,
+      (
+        lineNumber: number,
+        capture: RegExpExecArray,
+        offset: number,
+      ): SymbolInformation[] => [new SymbolInformation(
+        capture[1],
+        SymbolKind.Variable,
+        "",
+        new Location(
+          document.uri,
+          new Range(
+            new Position(
+              lineNumber,
+              offset + capture.index + (capture[0].length - capture[0].trim().length) - 1,
+            ),
+            new Position(
+              lineNumber,
+              offset + capture.index
+              + (capture[0].length - capture[0].trim().length)
+              + capture[1].length - 1,
+            ),
+          ),
+        ),
+      )],
+    );
+  }
+
+  private findRegularSymbols(document: TextDocument) : SymbolInformation[] {
+    const regex = /([\w\d]+|"(?:[^"\\]|\\.)*")(\s*(->|<-|--)\s*([\w\d]+|"(?:[^"\\]|\\.)*"))+/g;
+    const regexSymbol = /[\w\d]+|"(?:[^"\\]|\\.)*"/g;
+
+    return this.lineIterator(
+      document,
+      regex,
+      (
+        lineNumber: number,
+        captures: RegExpExecArray,
+        offset: number,
+      ) : SymbolInformation[] => {
+        const symbols: SymbolInformation[] = [];
+        let symbol;
+        // eslint-disable-next-line no-cond-assign
+        while ((symbol = regexSymbol.exec(captures[0])) != null) {
+          symbols.push(
+            new SymbolInformation(
+              symbol[0],
+              SymbolKind.Variable,
+              "",
+              new Location(
+                document.uri,
+                new Range(
+                  new Position(lineNumber, offset + symbol.index - 1),
+                  new Position(lineNumber, offset + symbol.index - 1 + symbol[0].length),
+                ),
+              ),
+            ),
+          );
+        }
+        return symbols;
+      },
+    );
+  }
+
+  public provideSymbols(document: TextDocument) : SymbolInformation[] {
+    let symbols : SymbolInformation[] = [];
+
+    symbols = symbols.concat(this.findExplicitNodeDefinition(document));
+    symbols = symbols.concat(this.findNodeDefinition(document));
+    symbols = symbols.concat(this.findRegularSymbols(document));
+    symbols = symbols.concat(this.findNodeDefinitionWithConfig(document));
+
+    return symbols;
+  }
+
+  public provideDocumentSymbols(
+    document: TextDocument,
+    // token: CancellationToken,
+  ): Promise<SymbolInformation[]> {
+    return Promise.resolve(this.provideSymbols(document));
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private containingSymbol(symbols: SymbolInformation[], position: Position) {
+    let renameSymbol: SymbolInformation|undefined;
+    symbols.forEach((symbol) => {
+      if (symbol.location.range.contains(position)) {
+        renameSymbol = symbol;
+      }
+    });
+    return renameSymbol;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public provideRenameEdits(
+    document: TextDocument,
+    position: Position,
+    newName: string,
+  // token: CancellationToken,
+  ):
+    Promise<WorkspaceEdit> {
+    return this.provideDocumentSymbols(document).then((symbols) => {
+      const edit:WorkspaceEdit = new WorkspaceEdit();
+
+      const renameSymbol = this.containingSymbol(symbols, position);
+      if (!renameSymbol) {
+        return Promise.reject();
+      }
+
+      symbols.forEach((symbol) => {
+        if (renameSymbol?.name === symbol.name) {
+          edit.replace(
+            document.uri,
+            symbol.location.range,
+            newName,
+          );
+        }
+      });
+
+      return Promise.resolve(edit);
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public provideReferences(
+    document: TextDocument,
+    position: Position,
+    // options: { includeDeclaration: boolean },
+    // token: CancellationToken,
+  ):
+    Promise<Location[]> {
+    return this.provideDocumentSymbols(document).then((symbols) => {
+      const findSymbol = this.containingSymbol(symbols, position);
+      if (!findSymbol) {
+        return Promise.reject();
+      }
+      const locations : Location[] = [];
+      symbols.forEach((symbol) => {
+        if (findSymbol?.name === symbol.name) {
+          locations.push(symbol.location);
+        }
+      });
+      return Promise.resolve(locations);
+    });
+  }
+}
